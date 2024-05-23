92b558c2940aa492b064b3e56c8f20d9c2d069f9
Subaiyal Sheikh <subaiyalshk@gmail.com>
2024-05-23 17:56:00 -0500

    refactored node creation

--- index.ts
+++ index.ts
@@ -11,14 +11,10 @@ app.use(express.json()); // for parsing application/json
 // // create a filesystem on top of Helia, in this case it's UnixFS
 // const fs = unixfs(node)
 
-async function StartIPFSNode(){
+async function main () {
   node = await createHelia()
   console.info('Helia is running')
   console.info('PeerId: ', node?.libp2p?.peerId?.toString())
-}
-
-async function main () {
- await StartIPFSNode()
 
   app.post('/upload', async (req, res) => {
       const message = req.body;
