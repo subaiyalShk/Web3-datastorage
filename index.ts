44558dc7f3752d233a60ef3233b670656274ada3
Subaiyal Sheikh <subaiyalshk@gmail.com>
2024-05-22 21:45:04 -0500

    issue with cid parsing resolved

--- index.ts
+++ index.ts
@@ -3,44 +3,47 @@ import { createHelia } from 'helia'
 import { json } from '@helia/json'
 import { unixfs } from '@helia/unixfs'
 import { addData } from './utils.js'
+import { CID } from 'multiformats/cid'
 
-let node; 
-let j; 
+let node;
 const app = express();
 app.use(express.json()); // for parsing application/json
 // // create a filesystem on top of Helia, in this case it's UnixFS
 // const fs = unixfs(node)
 
-async function main () {
+async function StartIPFSNode(){
   node = await createHelia()
-  j = json(node)
-  console.info('helia node created')
-  console.info('PeerId:', node?.libp2p?.peerId?.toString())
-  const cid = await j.add({ hello: 'world' })
-  const data = await j.get(cid);
-  console.log(data);
-    // Upload data to IPFS
+  console.info('Helia is running')
+  console.info('PeerId: ', node?.libp2p?.peerId?.toString())
+}
+
+async function main () {
+  StartIPFSNode()
+  // const cid = await j.add({ hello: 'world' })
+  // const data = await j.get(cid);
+  // console.log(data);
+
+  // Upload data to IPFS
   app.post('/upload', async (req, res) => {
       const message = req.body;
       console.log(message);
-      const cid = await j.add({ hello: 'world' })
-      res.json({"cid": cid});
+      const cid = await json(node).add({ hello: 'world' })
+      res.json({"cid": cid.toString()});
   });
 
   app.get('/read', async (req, res) => {
       try {
-        const cid = req.query.cid;
-        console.log(cid)
+        let cid = req.query.cid;
         if (!cid) {
           return res.status(400).json({ error: 'Missing cid parameter' });
         }
-        console.log(cid.toString())
+        cid = CID.parse(cid);
         // The below line is not working and breaks the code; possibly due to scoping issue
-        // const data = await j.get(cid);
-        // if (!data) {
-        //     return res.status(404).json({ error: 'Data not found' });
-        // }
-        res.status(200).json({ data: 'data' });
+        const data = await json(node).get(cid);
+        if (!data) {
+            return res.status(404).json({ error: 'Data not found' });
+        }
+        res.status(200).json({ data: data });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to read data' });
